b70a063dae742ee2c8c0583cd1bc776bae98987d
Reymkasten <163716776+Reymkasten@users.noreply.github.com>
2025-03-01 08:57:16 +0100

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 99);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Verify";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(591, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(591, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(246, 207);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(257, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(586, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 91);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.SymbolStore;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private List<(string Question, string Answer)> questions = new List<(string, string)>
+        {
+            ("What is the capital of Ukraine?", "kyiv"),
+            ("Is it true that earth orbits around the sun?", "yes"),
+            ("Who was the first person to land on the moon?", "neil armstrong")
+        };
+        private List<(string Question, string UserAnswer, string CorrectAnswer)> answerHistory = new List<(string, string, string)>();
+        private int Index = 0;
+        private int Answers = 0;
+        private int Attempt = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Load(null, null);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userAnswer = textBox1.Text.ToLower();
+            if (userAnswer == questions[Index].Answer)
+            {
+                Answers++;
+                label4.BackColor = Color.Green;
+                answerHistory.Add((questions[Index].Question, userAnswer, questions[Index].Answer));
+                Index++;
+                Attempt = 0;
+            }
+            else
+            {
+                label4.BackColor = Color.Red;
+                answerHistory.Add((questions[Index].Question, userAnswer, questions[Index].Answer));
+                Attempt++;
+            }
+            if (Index < questions.Count)
+            {
+                Form1_Load(null, null);
+            }
+            else
+            {
+                MessageBox.Show("Quiz completed!");
+                AnswerHistory();
+                var result = MessageBox.Show("Do you want to start over?", "Restart", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    Index = 0;
+                    Answers = 0;
+                    Attempt = 0;
+                    label4.BackColor = Color.White;
+                    answerHistory.Clear();
+                    Form1_Load(null, null);
+                }
+                else
+                {
+                    Close();
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Index = 0;
+            Answers = 0;
+            Attempt = 0;
+            answerHistory.Clear();
+            Form1_Load(null, null);
+            label4.BackColor = Color.White;
+        }
+        private void AnswerHistory()
+        {
+            string history = "Answer History:\n";
+            foreach (var entry in answerHistory)
+            {
+                history += $"Question: {entry.Question}\nYour Answer: {entry.UserAnswer}\nCorrect Answer: {entry.CorrectAnswer}\n\n";
+            }
+            MessageBox.Show(history, "Answer History");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = questions[Index].Question;
+            label2.Text = $"Correct Answers: {Answers}";
+            label3.Text = $"Attempts: {Attempt}";
+            textBox1.Clear();
+        }
+    }
+}
