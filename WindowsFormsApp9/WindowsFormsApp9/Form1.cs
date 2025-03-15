ebf21f416391e4e5a3172453cbfe2c4562b0b0dd
Reymkasten <163716776+Reymkasten@users.noreply.github.com>
2025-03-15 11:40:25 +0100

    Add files via upload

---
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    
+    public partial class Form1 : Form
+    {
+        private short counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length > counter)
+            {
+                counter++;
+            }
+            label1.Text = $"Characters: {counter}";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == ' ')
+            {
+               e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                if (textBox1.Text.Length > 0)
+                {
+                    counter--;
+                }
+                e.Handled = true; 
+            }
+        }
+    }
+}
